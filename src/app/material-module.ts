b0712d5e940c59dd89ea568982612c905cbc5757
germanheinz <heinz.german@gmail.com>
2020-06-10 15:13:56 +0200

    Login - Register Pages

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -1,13 +1,25 @@
 import {NgModule} from '@angular/core';
-import {MatSliderModule} from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 
 @NgModule({
   exports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatButtonModule,
+    MatButtonToggleModule,
   ]
 })
 export class MaterialModule {}
