ed044fef62143a8903373f5470de665fba89a033
germanheinz <heinz.german@gmail.com>
2020-06-11 15:44:10 +0200

    NGRX - Actions - Reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ServicesModule } from './services/services.module';
 
+// NGRX - REDUX
+import { StoreModule } from '@ngrx/store';
+import { appReducers } from './app.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +27,12 @@ import { ServicesModule } from './services/services.module';
     PagesModule,
     MaterialModule,
     ReactiveFormsModule,
-    ServicesModule
+    ServicesModule,
+    StoreModule.forRoot( appReducers ),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/ui.actions.ts
@@ -0,0 +1,6 @@
+// Snippet ngrx-actions + tab
+
+import { createAction } from '@ngrx/store';
+
+export const isLoading   = createAction('[UI Component] isLoading');
+export const stopLoading = createAction('[UI Component] stopLoading');
